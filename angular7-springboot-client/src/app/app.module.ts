f64e38e40fb9a38dfdc3e7bd735f0b7acd45efc3
David-B-Comer <davec2233@gmail.com>
2021-01-17 19:47:48 -0500

    added employee list

--- angular7-springboot-client/src/app/app.module.ts
+++ angular7-springboot-client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateEmployeeComponent,
-    EmployeeDetailsComponent
+    EmployeeDetailsComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule
