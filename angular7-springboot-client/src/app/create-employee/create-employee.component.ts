d952b6a79846bfc35c175943736d9d47eb3ba229
David-B-Comer <davec2233@gmail.com>
2021-01-18 19:19:35 -0500

    added create employee component ts

--- angular7-springboot-client/src/app/create-employee/create-employee.component.ts
+++ angular7-springboot-client/src/app/create-employee/create-employee.component.ts
@@ -1,15 +1,35 @@
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+selector: 'app-create-employee',
+templateUrl: './create-employee.component.html',
+styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+employee: Employee = new Employee();
+submitted = false;
 
-  ngOnInit(): void {
+constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
   }
 
+  newEmployee(): void {
+    this.submitted = false;
+    this.employee = new Employee();
+  }
+
+  save() {
+    this.employeeService.createEmployee(this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.employee = new Employee();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 }
